6eea762a7530b3f2cfcdb8fd90dabd0cfecb8530
João Aires <airesjoao25@outlook.com>
2020-01-19 13:59:33 -0300

    ajustment in about component

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,14 +3,14 @@
         <nav-bar title="Sobre" />
 
         <div class="about-text">
-            <p>Olá, meu nome é João Aires, sou formado em análise e desnvolvimento de sistemas (Uninter) e trabalho como desenvolvedor web e mobile. Tenho experiência com php, framework laravel, bancos de dados redis e mysql para manutenção, desenvolvimento e consumo de api's REST, html, css, javascript, bibliotecas Vue,Vuex para desenvolvimento e manutenção de painéis administrativos. No desenvolvimento mobile trabalho com React, React Native e Redux.</p>
+            <p>Olá, meu nome é João Aires, sou formado em análise e desnvolvimento de sistemas (Uninter) e trabalho como desenvolvedor web e mobile. Tenho experiência com php, framework laravel, bancos de dados redis e mysql para manutenção, desenvolvimento e consumo de api's REST, html, css, javascript, bibliotecas Vue,Vuex para desenvolvimento e manutenção de interfaces. No desenvolvimento mobile trabalho com React, React Native e Redux.</p>
         </div>
         <div class="about-tech">
-            <p>Backend: PHP, framework Laravel.</p>
-            <p>Bancos de dados: Redis, MySql.</p>
-            <p>Frontend: Html, Css, JavaScript, bibliotecas Vue, Vuex, React, React Native e Redux.</p>
-            <p>Versionamento de software: Git, GitHub, GitLab.</p>
-        </div>
+            <p>Backend: <span class="php">PHP</span>, framework <span class="laravel">Laravel</span>.</p>
+            <p>Bancos de dados: <span class="redis">Redis</span>, <span class="mysql">MySql</span>.</p>
+            <p>Frontend: <span class="html">Html</span>, <span class="css">Css</span>, <span class="js">JavaScript</span>, bibliotecas <span class="vue">Vue</span>, <span class="vue">Vuex</span>, <span class="react">React</span>, <span class="react">React Native</span> e <span class="redux">Redux</span>.</p>
+            <p>Versionamento de software: <span class="git">Git</span>.</p>
+        </div>  
     </div>
 </template>
 
@@ -39,6 +39,51 @@ export default {
     max-width: 1000px;
     margin: 0 auto;
     padding: 15px;
+}
+
+.php {
+    font-weight: bold;
+    color: mediumslateblue;
+}
+.laravel {
+    font-weight: bold;
+    color: #ff2d20;
+}
+.redis {
+    font-weight: bold;
+    color: #a32422;;
+}
+.mysql {
+    font-weight: bold;
+    color: blue;
+}
+.js {
+    font-weight: bold;
+    color: #FFCD3A;;
+}
+.vue {
+    font-weight: bold;
+    color: #42b983;
+}
+.react {
+    font-weight: bold;
+    color: #61dafb;
+}
+.redux {
+    font-weight: bold;
+    color: rgb(118, 74, 188);
+}
+.git {
+    font-weight: bold;
+    color: #f14e32;
+}
+.html {
+    font-weight: bold;
+    color: chocolate;
+}
+.css {
+    font-weight: bold;
+    color: fuchsia;
 }
 
 </style>
