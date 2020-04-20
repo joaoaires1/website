efeec0906a57114eaa912c77c79dd2a285be83d5
João Aires <airesjoao25@outlook.com>
2020-04-20 20:13:29 -0300

    ajustments in texts

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,9 +3,11 @@
         <nav-bar title="Sobre" />
 
         <div class="about-text">
-            <p>Olá, meu nome é João Aires, sou formado em análise e desenvolvimento de sistemas (Uninter) e trabalho como desenvolvedor web e mobile. Tenho experiências com php, laravel framework, bancos de dados redis e mysql atuando no desenvolvimento, manutenção e consumo de api's REST. Html, css, javascript, bibliotecas Vue,Vuex e React para desenvolvimento e manutenção de interfaces. No desenvolvimento mobile trabalho com React Native.</p>
+            <p>Olá, meu nome é João Aires, sou desenvolvedor web e tenho tecnólogo em análise e desenvolvimento de sistemas (Uninter). Minhas principais experiências proficionais são com php, framework laravel e javascript com as bibliotecas vue, react e react native.</p>
         </div>
         <div class="about-tech">
+            <h2>Habilidades:</h2>
+            <br />
             <p>Backend: <span class="php">PHP</span>, <span class="laravel">Laravel</span> framework.</p>
             <p>Bancos de dados: <span class="redis">Redis</span>, <span class="mysql">MySql</span>.</p>
             <p>Frontend: <span class="html">Html</span>, <span class="css">Css</span>, <span class="js">JavaScript</span>, bibliotecas <span class="vue">Vue</span>, <span class="vue">Vuex</span>, <span class="react">React</span>, <span class="react">React Native</span> e <span class="redux">Redux</span>.</p>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,10 +7,7 @@
                     <p>Olá! Eu sou João Aires,</p>
                 </div>
                 <div>
-                    <h1>Desenvolvedor</h1>
-                </div>
-                <div>
-                    <h1><span class="tech">Web</span> e <span class="tech">Mobile</span>.</h1>
+                    <h1>Desenvolvedor <span class="tech">Web</span></h1>
                 </div>
                 <div class="nav">
                     <p><span class="link" @click="navigate('projects')">Projetos</span> - <span class="link" @click="navigate('about')">Sobre</span> - <span class="link"><a href="https://github.com/joaoaires1" target="_blank">GitHub</a></span> - <span class="link"><a href="https://www.linkedin.com/in/joaoaires1/" target="_blank" >Linkedin</a></span></p>
