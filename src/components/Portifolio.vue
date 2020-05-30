b4bb0179218cb2cd91fa47e389785198755377dc
João Aires <airesjoao25@outlook.com>
2020-05-30 11:52:24 -0300

    ajustments in projects

--- src/components/Portifolio.vue
+++ src/components/Portifolio.vue
@@ -4,14 +4,50 @@
         <nav-bar title="Projetos" />
 
         <div class="projects">
-            <div v-for="item in list" :key="item.id" class="card">
+            <div class="card">
 
-                <div class="card-cover">
-
+                <div class="card-cover bg-green">
+                    <img class="cover-img" src="../assets/twitter_brand.png" alt="Twitter logo">
                 </div>
 
                 <div class="card-text">
-                    {{ item.description }}
+                    SPA Twitter type application desenvolvida usando vuejs, vuex e vue-router. <a target="_blank" href="https://ttclone.netlify.app/">link da applicação</a>
+                </div>
+
+            </div>
+
+            <div class="card">
+
+                <div class="card-cover bg-laravel">
+                    <img class="cover-img" src="../assets/twitter_brand.png" alt="Twitter logo">
+                </div>
+
+                <div class="card-text">
+                    API Twitter type application desenvolvida usando PHP com framework Laravel.<a target="_blank" href="https://github.com/joaoaires1/ttclone_api">link no github</a>
+                </div>
+
+            </div>
+
+            <div class="card">
+
+                <div class="card-cover bg-react">
+                    <img class="cover-img" src="../assets/supermarket.png" alt="Twitter logo">
+                </div>
+
+                <div class="card-text">
+                    Implementação de um carrinho de compras utilizando react hooks e redux.<a target="_blank" href="https://shopcarthooks.netlify.app/">link da applicação</a>
+                </div>
+
+            </div>
+
+            <div class="card">
+
+                <div class="card-cover bg-react">
+                    <img class="cover-img" src="../assets/twitter_brand.png" alt="Twitter logo">
+                </div>
+
+                <div class="card-text">
+                    Mobile app Twitter type application desenvolvida utilizando react native.<a target="_blank" href="https://github.com/joaoaires1/ttclone_app">link no github</a>
                 </div>
 
             </div>
@@ -29,20 +65,7 @@ export default {
     },
     data () {
         return {
-            list: [
-                {
-                    id: 0,
-                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-                },
-                {
-                    id: 1,
-                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-                },
-                {
-                    id: 2,
-                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-                }
-            ]
+
         }
     }
 }
@@ -50,13 +73,18 @@ export default {
 
 <style>
 
+@media only screen and (max-width: 900px) {
+  .projects {
+    justify-content: center;
+  }
+}
+
 .projects {
     width: 100%;
     max-width: 1000px;
     margin: 0 auto;
     display: flex;
     flex-flow: row wrap;
-    justify-content: center;
 }
 
 .card {
@@ -68,13 +96,31 @@ export default {
 
 .card-cover {
     /* width: 100%; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
     height: 210px;
-    background: blue;
+}
+
+.bg-green {
+    background: green;
+}
+
+.bg-laravel {
+    background: #ff2d20;
+}
+.bg-react {
+    background: #61DBFB;
 }
 
 .card-text {
     padding: 10px;
     text-align: justify;
+}
+
+.cover-img {
+    width: 120px;
+    height: 120px;
 }
 
 </style>
